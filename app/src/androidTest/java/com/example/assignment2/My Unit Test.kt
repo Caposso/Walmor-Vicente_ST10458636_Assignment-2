4edcedf0a87b4f4a05da2e2bcad71a6fa1d5114c
Caposso <st10458636@vcconnect.edu.za>
2024-05-05 17:18:14 +0200

    Work Finished

---
+++ app/src/androidTest/java/com/example/assignment2/My Unit Test.kt
@@ -0,0 +1,31 @@
+package com.example.assignment2
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun clickButton_opensGameActivity() {
+        // Click the button
+        onView(withId(R.id.button)).perform(click())
+
+        // Check if Game Activity is opened
+        onView(withId(R.id.imageView)).check(matches(isDisplayed()))
+    }
+}
